5407b387c32e52b795a5e5dd4a855f70796a3d33
ushu3323 <redspower10@gmail.com>
2022-12-10 04:26:44 -0300

    Setup swagger-ui-express to /docs route app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Delilah Resto NestJS')
+    .setDescription('API description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   const prismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
