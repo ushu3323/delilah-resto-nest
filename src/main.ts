3a19ddc37eeeaf755bca7039bf64a5df092a7d66
ushu3323 <redspower10@gmail.com>
2022-12-13 03:18:42 -0300

    Bind class-validation pipe and class-transform interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const config = new DocumentBuilder()
     .setTitle('Delilah Resto NestJS')
