411e9e8c28735187a2bd8565a3ac7855a3de7778
ushu3323 <redspower10@gmail.com>
2022-12-15 18:47:49 -0300

    Add class-validator constraints as app providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { IsEmailUniqueConstraint } from './decorators/class-validator/IsEmailUnique';
+import { IsUsernameUniqueConstraint } from './decorators/class-validator/IsUsernameUnique';
+
+const classValidatorConstraints = [
+  IsEmailUniqueConstraint,
+  IsUsernameUniqueConstraint,
+];
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, ...classValidatorConstraints],
 })
 export class UsersModule {}
