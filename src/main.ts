0e8bf5b062735258dadccf92b65292ad338a14cf
ushu3323 <redspower10@gmail.com>
2022-12-15 01:29:10 -0300

    Bind prisma-exception-filter globally

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './prisma/exceptions/prisma-client-exception/prisma-client-exception.filter';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
@@ -9,6 +10,9 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   const config = new DocumentBuilder()
     .setTitle('Delilah Resto NestJS')
