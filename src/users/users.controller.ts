93612b28a368f286fc70b1f16b71ddee25dcc78e
ushu3323 <redspower10@gmail.com>
2022-12-15 17:44:37 -0300

    Send not found response on users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -27,20 +28,26 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findOne(id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const user = await this.usersService.findOne(id);
+    if (!user) throw new NotFoundException();
+    return user;
   }
 
   @Patch(':id')
-  update(
+  async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
   ) {
-    return this.usersService.update(id, updateUserDto);
+    const user = await this.usersService.update(id, updateUserDto);
+    if (!user) throw new NotFoundException();
+    return user;
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.remove(id);
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    const user = await this.usersService.remove(id);
+    if (!user) throw new NotFoundException();
+    return user;
   }
 }
