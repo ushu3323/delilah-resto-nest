e33013b8bf9b1ea787c1ba0294bbe58d0ff99079
ushu3323 <redspower10@gmail.com>
2022-12-15 17:47:34 -0300

    Define create-user dto with validations

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,44 @@
-export class CreateUserDto {}
+import {
+  IsAlphanumeric,
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).*$/g;
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 50)
+  first_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 50)
+  last_name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @Length(6, 20)
+  @IsAlphanumeric()
+  username: string;
+
+  @IsNotEmpty()
+  @Length(8, 40)
+  @Matches(PASSWORD_REGEX, {
+    message:
+      'must contain one lowercase word, one uppercase word and one number',
+  })
+  password: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber()
+  phone_number: string;
+}
