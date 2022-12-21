bbc53335a02bf501499bee8ce1636ff7f17cffab
ushu3323 <redspower10@gmail.com>
2022-12-21 00:20:23 -0300

    Document user controller operations and responses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,41 +13,57 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserEntity } from './entities/user.entity';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { BadRequestEntity } from '../common/entities/bad-request.entity';
 
+@ApiTags('Users')
+@ApiBadRequestResponse({ type: BadRequestEntity })
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  @ApiOperation({ summary: 'Create user' })
+  async create(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
     return new UserEntity(await this.usersService.create(createUserDto));
   }
 
   @Get()
-  async findAll() {
+  @ApiOperation({ summary: 'Get all users' })
+  async findAll(): Promise<UserEntity[]> {
     const users = await this.usersService.findAll();
     return users.map((user) => new UserEntity(user));
   }
 
   @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number) {
+  @ApiOperation({ summary: 'Get an user' })
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<UserEntity> {
     const user = await this.usersService.findOne(id);
     if (!user) throw new NotFoundException();
     return new UserEntity(user);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update user' })
+  @ApiOkResponse({ type: UserEntity })
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
-  ) {
+  ): Promise<UserEntity> {
     const user = await this.usersService.update(id, updateUserDto);
     if (!user) throw new NotFoundException();
     return new UserEntity(user);
   }
 
   @Delete(':id')
-  async remove(@Param('id', ParseIntPipe) id: number) {
+  @ApiOperation({ summary: 'Remove an user' })
+  @ApiOkResponse({ type: UserEntity })
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<UserEntity> {
     const user = await this.usersService.remove(id);
     if (!user) throw new NotFoundException();
     return new UserEntity(user);
