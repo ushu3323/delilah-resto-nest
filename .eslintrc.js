e5c5e321161bb4be4c0e6e5da615fb254162b214
ushu3323 <redspower10@gmail.com>
2022-12-21 00:25:49 -0300

    Add "prettier" plugin, and "import" plugin and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,16 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier', 'import'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   root: true,
   env: {
@@ -21,5 +24,28 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'sort-imports': ['warn', {
+      ignoreCase: false,
+      ignoreDeclarationSort: true,
+      ignoreMemberSort: false,
+      memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      allowSeparatedGroups: true,
+    }],
+    'import/no-unresolved': 'error',
+    'import/order': ['warn', {
+      groups: [
+        'builtin',
+        'external',
+        'internal',
+        ['sibling', 'parent'],
+        'index',
+        'unknown',
+      ],
+      'newlines-between': 'always',
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true,
+      },
+    }]
   },
 };
